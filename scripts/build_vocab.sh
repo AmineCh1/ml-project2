73f881f454d86420d9e3b4b02414f4d03425f18d
David Dieulivol <david.dieulivol@epfl.ch>
2020-11-06 20:36:16 +0100

    What it took for my local setup to work :)

--- scripts/build_vocab.sh
+++ scripts/build_vocab.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Note that this script uses GNU-style sed. On Mac OS, you are required to first
-#    brew install gnu-sed --with-default-names
-cat ../data/pos_train.txt ../data/neg_train.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > ../data/vocab/vocab.txt
+#    brew install gnu-sed
+#
+# and then change your PATH to
+#    PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+cat ../data/train_pos.txt ../data/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > ../data/vocab/vocab.txt
