ae31a8b8766ce897dc7ef500cabec50554ab4225
David Dieulivol <david.dieulivol@epfl.ch>
2020-11-28 11:03:37 +0100

    Cleanup of older code

--- scripts/build_vocab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Note that this script uses GNU-style sed. On Mac OS, you are required to first
-#    brew install gnu-sed
-#
-# and then change your PATH to
-#    PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-cat ../data/train_pos.txt ../data/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > ../data/vocab/vocab.txt
--- scripts/cut_vocab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Note that this script uses GNU-style sed. On Mac OS, you are required to first
-#    brew install gnu-sed
-#
-# and then change your PATH to
-#    PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-cat ../data/vocab/vocab.txt | sed "s/^\s\+//g" | sort -rn | grep -v "^[1234]\s" | cut -d' ' -f2 > ../data/vocab/vocab_cut.txt
